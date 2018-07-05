f4b5549cc6ad8ba385ee96a02e52ef09d95d6b38
unknown <marullia98@gmail.com>
2018-07-05 11:13:34 +0900

    'getblocktemplate' rpc command update

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -150,7 +150,7 @@ extern json_spirit::Value importprivkey(const json_spirit::Array& params, bool f
 // extern json_spirit::Value getmininginfo(const json_spirit::Array& params, bool fHelp);
 // extern json_spirit::Value getworkex(const json_spirit::Array& params, bool fHelp);
 // extern json_spirit::Value getwork(const json_spirit::Array& params, bool fHelp);
-// extern json_spirit::Value getblocktemplate(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getblocktemplate(const json_spirit::Array& params, bool fHelp);
 // extern json_spirit::Value submitblock(const json_spirit::Array& params, bool fHelp);
 
 extern json_spirit::Value getnewaddress(const json_spirit::Array& params, bool fHelp); // in rpcwallet.cpp
